07ff17d951261442a3fbec3a100a3c861b15f39d
y-iwamoto <klc.oka.iwa.tsu.gmiw@gmail.com>
2025-04-12 21:05:37 +0900

    fix: リストのスタイリングと要素間の視覚的区分を改善

--- frontend/src/Test.tsx
+++ frontend/src/Test.tsx
@@ -23,13 +23,19 @@ function Test() {
   return (
     <div>
       <h1>テスト画面</h1>
-
-      <ul>
+      <ul style={{ paddingInlineStart: 0, listStyle: 'none' }}>
+        {data?.users.length === 0 && <li>ユーザーが見つかりませんでした</li>}
         {data?.users.map((user) => (
-          <li key={user.id}>
-            <span>{user.email}</span>
-            <span>{user.firstName}</span>
-            <span>{user.lastName}</span>
+          <li key={user.id} style={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'space-between',
+            alignItems: 'flex-start',
+            borderBottom: '1px solid #eee',
+            padding: '10px 0'
+          }}>
+            <div><strong>メール:</strong> {user.email}</div>
+            <div><strong>名前:</strong> {user.firstName} {user.lastName}</div>
           </li>
         ))}
       </ul>
