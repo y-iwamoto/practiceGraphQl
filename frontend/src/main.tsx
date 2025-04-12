0f11d17a37b7363865fc1cfdfd3ef3601be07c52
y-iwamoto <klc.oka.iwa.tsu.gmiw@gmail.com>
2025-04-12 21:13:02 +0900

    fix: 非nullアサーション（!）の使用は避け修正

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -7,7 +7,12 @@ import './index.css'
 import App from './App.tsx'
 import Test from './Test.tsx'
 
-createRoot(document.getElementById('root')!).render(
+const rootElement = document.getElementById('root');
+if (!rootElement) {
+  throw new Error('rootが見つかりませんでした');
+}
+
+createRoot(rootElement).render(
   <StrictMode>
     <ApolloProvider client={client}>
       <BrowserRouter>
