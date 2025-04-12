ac01b8010274d2210a33ca460e5e8d08e131b539
y-iwamoto <klc.oka.iwa.tsu.gmiw@gmail.com>
2025-04-12 21:49:44 +0900

    feat: バックエンド側でユーザ新規登録機能を追加

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  app.enableCors({
+    origin: ['http://localhost:5174'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- backend/src/user/user.resolver.ts
+++ backend/src/user/user.resolver.ts
@@ -1,6 +1,7 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { User } from './user.entity';
 import { UserService } from './user.service';
+import { CreateUserInput } from '@/user/dto/create-user.input';
 
 @Resolver(() => User)
 export class UserResolver {
@@ -9,5 +10,19 @@ export class UserResolver {
   @Query(() => [User])
   async users() {
     return this.userService.findAll();
+  }
+
+  @Mutation(() => User)
+  async createUser(@Args('createUserInput') createUserInput: CreateUserInput) {
+    const isEmailExists = await this.userService.checkEmail(
+      createUserInput.email,
+    );
+    if (isEmailExists) {
+      throw new Error(
+        '入力されたメールアドレスのアカウントが既に存在する可能性があります',
+      );
+    }
+
+    return this.userService.create(createUserInput);
   }
 }
