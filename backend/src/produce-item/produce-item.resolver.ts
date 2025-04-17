1eae3e8289917fdc4900bfd8f134ff7781a72216
y-iwamoto <klc.oka.iwa.tsu.gmiw@gmail.com>
2025-04-17 21:34:20 +0900

    chore: 今実装されていないqueryやmutaion処理を削除

--- backend/src/produce-item/produce-item.resolver.ts
+++ backend/src/produce-item/produce-item.resolver.ts
@@ -1,8 +1,7 @@
-import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
+import { Resolver, Mutation, Args } from '@nestjs/graphql';
 import { ProduceItemService } from './produce-item.service';
 import { ProduceItem } from './entities/produce-item.entity';
 import { CreateProduceItemInput } from './dto/create-produce-item.input';
-import { UpdateProduceItemInput } from './dto/update-produce-item.input';
 import { FarmOwnershipGuard } from '@/auth/guards/farm-ownership.guard';
 import { UseGuards } from '@nestjs/common';
 
@@ -19,23 +18,4 @@ export class ProduceItemResolver {
     return this.produceItemService.create(createProduceItemInput);
   }
 
-  @Query(() => [ProduceItem], { name: 'produceItem' })
-  findAll() {
-    return this.produceItemService.findAll();
-  }
-
-  @Query(() => ProduceItem, { name: 'produceItem' })
-  findOne(@Args('id', { type: () => Int }) id: number) {
-    return this.produceItemService.findOne(id);
-  }
-
-  @Mutation(() => ProduceItem)
-  updateProduceItem(@Args('updateProduceItemInput') updateProduceItemInput: UpdateProduceItemInput) {
-    return this.produceItemService.update(updateProduceItemInput.id, updateProduceItemInput);
-  }
-
-  @Mutation(() => ProduceItem)
-  removeProduceItem(@Args('id', { type: () => Int }) id: number) {
-    return this.produceItemService.remove(id);
-  }
 }
