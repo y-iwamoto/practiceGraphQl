843a422089c42df9e31d285883605240f6ac51ca
y-iwamoto <klc.oka.iwa.tsu.gmiw@gmail.com>
2025-04-18 18:34:46 +0900

    chore: Playground からの スキーマ取得（GET）リクエスト に対して AuthMiddleware をスキップさせる対応追加

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,4 +1,9 @@
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { join } from 'path';
@@ -46,6 +51,9 @@ import { ProduceStockModule } from './produce-stock/produce-stock.module';
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer.apply(AuthMiddleware).forRoutes('graphql');
+    consumer
+      .apply(AuthMiddleware)
+      .exclude({ path: 'graphql', method: RequestMethod.GET })
+      .forRoutes('graphql');
   }
 }
