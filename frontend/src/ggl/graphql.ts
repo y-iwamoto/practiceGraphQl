9f56711a1e1433f6a189686043a378811fec02e5
y-iwamoto <klc.oka.iwa.tsu.gmiw@gmail.com>
2025-05-02 07:11:50 +0900

    feat: order作成画面を実装

--- frontend/src/ggl/graphql.ts
+++ frontend/src/ggl/graphql.ts
@@ -28,6 +28,20 @@ export type CreateFarmInput = {
   restAddress: Scalars['String']['input'];
 };
 
+export type CreateOrderInput = {
+  farmId: Scalars['Int']['input'];
+  produceItems: Array<ProduceItemOrderInput>;
+};
+
+export type CreateProduceItemInput = {
+  farmId: Scalars['Int']['input'];
+  name: Scalars['String']['input'];
+};
+
+export type CreateShipmentInput = {
+  orderId: Scalars['Int']['input'];
+};
+
 export type CreateUserInput = {
   email: Scalars['String']['input'];
   firstName: Scalars['String']['input'];
@@ -41,9 +55,12 @@ export type Farm = {
   createdAt?: Maybe<Scalars['DateTime']['output']>;
   id: Scalars['ID']['output'];
   name: Scalars['String']['output'];
+  orders: Array<Order>;
   owner: User;
   postalCode: Scalars['String']['output'];
   prefecture: Scalars['String']['output'];
+  produceItems: Array<ProduceItem>;
+  produceStocks: Array<ProduceStock>;
   restAddress: Scalars['String']['output'];
   updatedAt?: Maybe<Scalars['DateTime']['output']>;
 };
@@ -51,7 +68,12 @@ export type Farm = {
 export type Mutation = {
   __typename?: 'Mutation';
   createFarm: Farm;
+  createOrder: Order;
+  createProduceItem: ProduceItem;
+  createShipment: Shipment;
   createUser: User;
+  updateProduceStock: ProduceStock;
+  updateShipment: Shipment;
 };
 
 
@@ -60,13 +82,148 @@ export type MutationCreateFarmArgs = {
 };
 
 
+export type MutationCreateOrderArgs = {
+  createOrderInput: CreateOrderInput;
+};
+
+
+export type MutationCreateProduceItemArgs = {
+  createProduceItemInput: CreateProduceItemInput;
+};
+
+
+export type MutationCreateShipmentArgs = {
+  createShipmentInput: CreateShipmentInput;
+};
+
+
 export type MutationCreateUserArgs = {
   createUserInput: CreateUserInput;
 };
 
+
+export type MutationUpdateProduceStockArgs = {
+  updateProduceStockInput: UpdateProduceStockInput;
+};
+
+
+export type MutationUpdateShipmentArgs = {
+  updateShipmentInput: UpdateShipmentInput;
+};
+
+export type Order = {
+  __typename?: 'Order';
+  buyer: User;
+  buyerId: Scalars['Int']['output'];
+  createdAt?: Maybe<Scalars['DateTime']['output']>;
+  farm: Farm;
+  farmId: Scalars['Int']['output'];
+  id: Scalars['Int']['output'];
+  orderDetails: Array<OrderDetail>;
+  orderedAt: Scalars['DateTime']['output'];
+  shipment?: Maybe<Shipment>;
+  status: OrderStatus;
+  updatedAt?: Maybe<Scalars['DateTime']['output']>;
+};
+
+export type OrderDetail = {
+  __typename?: 'OrderDetail';
+  amount: Scalars['Int']['output'];
+  createdAt?: Maybe<Scalars['DateTime']['output']>;
+  id: Scalars['Int']['output'];
+  order: Order;
+  orderId: Scalars['Int']['output'];
+  produceItem: ProduceItem;
+  produceItemId: Scalars['Int']['output'];
+  updatedAt?: Maybe<Scalars['DateTime']['output']>;
+};
+
+/** 注文ステータス */
+export enum OrderStatus {
+  Cancelled = 'CANCELLED',
+  Confirmed = 'CONFIRMED',
+  Delivered = 'DELIVERED',
+  Pending = 'PENDING',
+  Shipped = 'SHIPPED'
+}
+
+export type ProduceItem = {
+  __typename?: 'ProduceItem';
+  createdAt?: Maybe<Scalars['DateTime']['output']>;
+  farm: Farm;
+  farmId: Scalars['Int']['output'];
+  id: Scalars['Int']['output'];
+  name: Scalars['String']['output'];
+  orderDetails: Array<OrderDetail>;
+  produceStock: ProduceStock;
+  updatedAt?: Maybe<Scalars['DateTime']['output']>;
+};
+
+export type ProduceItemOrderInput = {
+  amount: Scalars['Int']['input'];
+  produceItemId: Scalars['Int']['input'];
+};
+
+export type ProduceStock = {
+  __typename?: 'ProduceStock';
+  amount: Scalars['Int']['output'];
+  createdAt?: Maybe<Scalars['DateTime']['output']>;
+  farm: Farm;
+  farmId: Scalars['Int']['output'];
+  id: Scalars['Int']['output'];
+  produceItem: ProduceItem;
+  produceItemId: Scalars['Int']['output'];
+  updatedAt?: Maybe<Scalars['DateTime']['output']>;
+};
+
 export type Query = {
   __typename?: 'Query';
+  farm: Farm;
+  farms: Array<Farm>;
   users: Array<User>;
+};
+
+
+export type QueryFarmArgs = {
+  id: Scalars['Float']['input'];
+};
+
+/** ユーザーの権限 */
+export enum Role {
+  Admin = 'Admin',
+  Buyer = 'Buyer',
+  Farmer = 'Farmer'
+}
+
+export type Shipment = {
+  __typename?: 'Shipment';
+  createdAt: Scalars['DateTime']['output'];
+  deliveredAt?: Maybe<Scalars['DateTime']['output']>;
+  estimatedDeliveryDate?: Maybe<Scalars['DateTime']['output']>;
+  id: Scalars['Int']['output'];
+  order: Order;
+  orderId: Scalars['Int']['output'];
+  shippedAt?: Maybe<Scalars['DateTime']['output']>;
+  status: ShipmentStatus;
+  updatedAt: Scalars['DateTime']['output'];
+};
+
+/** 発送ステータス */
+export enum ShipmentStatus {
+  Delivered = 'DELIVERED',
+  Pending = 'PENDING',
+  Shipped = 'SHIPPED'
+}
+
+export type UpdateProduceStockInput = {
+  amount: Scalars['Int']['input'];
+  farmId: Scalars['Int']['input'];
+  id: Scalars['Int']['input'];
+};
+
+export type UpdateShipmentInput = {
+  id: Scalars['Int']['input'];
+  status: ShipmentStatus;
 };
 
 export type User = {
@@ -77,6 +234,8 @@ export type User = {
   firstName: Scalars['String']['output'];
   id: Scalars['ID']['output'];
   lastName: Scalars['String']['output'];
+  orders?: Maybe<Array<Order>>;
+  role: Role;
   updatedAt?: Maybe<Scalars['DateTime']['output']>;
 };
 
@@ -92,6 +251,20 @@ export type GetUsersQueryVariables = Exact<{ [key: string]: never; }>;
 
 export type GetUsersQuery = { __typename?: 'Query', users: Array<{ __typename?: 'User', id: string, email: string, firstName: string, lastName: string }> };
 
+export type CreateOrderMutationVariables = Exact<{
+  createOrderInput: CreateOrderInput;
+}>;
+
+
+export type CreateOrderMutation = { __typename?: 'Mutation', createOrder: { __typename?: 'Order', id: number, orderedAt: any, status: OrderStatus, buyer: { __typename?: 'User', id: string, firstName: string, lastName: string, email: string }, farm: { __typename?: 'Farm', id: string, name: string }, orderDetails: Array<{ __typename?: 'OrderDetail', id: number, amount: number, produceItem: { __typename?: 'ProduceItem', id: number, name: string } }> } };
+
+export type GetFarmRelatedItemQueryVariables = Exact<{
+  farmId: Scalars['Float']['input'];
+}>;
+
+
+export type GetFarmRelatedItemQuery = { __typename?: 'Query', farm: { __typename?: 'Farm', id: string, name: string, produceItems: Array<{ __typename?: 'ProduceItem', id: number, name: string, produceStock: { __typename?: 'ProduceStock', id: number, amount: number } }> } };
+
 export type CreateUserMutationVariables = Exact<{
   createUserInput: CreateUserInput;
 }>;
@@ -102,4 +275,6 @@ export type CreateUserMutation = { __typename?: 'Mutation', createUser: { __type
 
 export const CreateFarmDocument = {"kind":"Document","definitions":[{"kind":"OperationDefinition","operation":"mutation","name":{"kind":"Name","value":"CreateFarm"},"variableDefinitions":[{"kind":"VariableDefinition","variable":{"kind":"Variable","name":{"kind":"Name","value":"createFarmInput"}},"type":{"kind":"NonNullType","type":{"kind":"NamedType","name":{"kind":"Name","value":"CreateFarmInput"}}}}],"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"createFarm"},"arguments":[{"kind":"Argument","name":{"kind":"Name","value":"createFarmInput"},"value":{"kind":"Variable","name":{"kind":"Name","value":"createFarmInput"}}}],"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"id"}},{"kind":"Field","name":{"kind":"Name","value":"name"}},{"kind":"Field","name":{"kind":"Name","value":"postalCode"}},{"kind":"Field","name":{"kind":"Name","value":"prefecture"}},{"kind":"Field","name":{"kind":"Name","value":"city"}},{"kind":"Field","name":{"kind":"Name","value":"restAddress"}},{"kind":"Field","name":{"kind":"Name","value":"building"}},{"kind":"Field","name":{"kind":"Name","value":"owner"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"id"}},{"kind":"Field","name":{"kind":"Name","value":"email"}},{"kind":"Field","name":{"kind":"Name","value":"firstName"}},{"kind":"Field","name":{"kind":"Name","value":"lastName"}}]}}]}}]}}]} as unknown as DocumentNode<CreateFarmMutation, CreateFarmMutationVariables>;
 export const GetUsersDocument = {"kind":"Document","definitions":[{"kind":"OperationDefinition","operation":"query","name":{"kind":"Name","value":"GetUsers"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"users"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"id"}},{"kind":"Field","name":{"kind":"Name","value":"email"}},{"kind":"Field","name":{"kind":"Name","value":"firstName"}},{"kind":"Field","name":{"kind":"Name","value":"lastName"}}]}}]}}]} as unknown as DocumentNode<GetUsersQuery, GetUsersQueryVariables>;
+export const CreateOrderDocument = {"kind":"Document","definitions":[{"kind":"OperationDefinition","operation":"mutation","name":{"kind":"Name","value":"CreateOrder"},"variableDefinitions":[{"kind":"VariableDefinition","variable":{"kind":"Variable","name":{"kind":"Name","value":"createOrderInput"}},"type":{"kind":"NonNullType","type":{"kind":"NamedType","name":{"kind":"Name","value":"CreateOrderInput"}}}}],"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"createOrder"},"arguments":[{"kind":"Argument","name":{"kind":"Name","value":"createOrderInput"},"value":{"kind":"Variable","name":{"kind":"Name","value":"createOrderInput"}}}],"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"id"}},{"kind":"Field","name":{"kind":"Name","value":"orderedAt"}},{"kind":"Field","name":{"kind":"Name","value":"status"}},{"kind":"Field","name":{"kind":"Name","value":"buyer"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"id"}},{"kind":"Field","name":{"kind":"Name","value":"firstName"}},{"kind":"Field","name":{"kind":"Name","value":"lastName"}},{"kind":"Field","name":{"kind":"Name","value":"email"}}]}},{"kind":"Field","name":{"kind":"Name","value":"farm"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"id"}},{"kind":"Field","name":{"kind":"Name","value":"name"}}]}},{"kind":"Field","name":{"kind":"Name","value":"orderDetails"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"id"}},{"kind":"Field","name":{"kind":"Name","value":"amount"}},{"kind":"Field","name":{"kind":"Name","value":"produceItem"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"id"}},{"kind":"Field","name":{"kind":"Name","value":"name"}}]}}]}}]}}]}}]} as unknown as DocumentNode<CreateOrderMutation, CreateOrderMutationVariables>;
+export const GetFarmRelatedItemDocument = {"kind":"Document","definitions":[{"kind":"OperationDefinition","operation":"query","name":{"kind":"Name","value":"GetFarmRelatedItem"},"variableDefinitions":[{"kind":"VariableDefinition","variable":{"kind":"Variable","name":{"kind":"Name","value":"farmId"}},"type":{"kind":"NonNullType","type":{"kind":"NamedType","name":{"kind":"Name","value":"Float"}}}}],"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"farm"},"arguments":[{"kind":"Argument","name":{"kind":"Name","value":"id"},"value":{"kind":"Variable","name":{"kind":"Name","value":"farmId"}}}],"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"id"}},{"kind":"Field","name":{"kind":"Name","value":"name"}},{"kind":"Field","name":{"kind":"Name","value":"produceItems"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"id"}},{"kind":"Field","name":{"kind":"Name","value":"name"}},{"kind":"Field","name":{"kind":"Name","value":"produceStock"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"id"}},{"kind":"Field","name":{"kind":"Name","value":"amount"}}]}}]}}]}}]}}]} as unknown as DocumentNode<GetFarmRelatedItemQuery, GetFarmRelatedItemQueryVariables>;
 export const CreateUserDocument = {"kind":"Document","definitions":[{"kind":"OperationDefinition","operation":"mutation","name":{"kind":"Name","value":"CreateUser"},"variableDefinitions":[{"kind":"VariableDefinition","variable":{"kind":"Variable","name":{"kind":"Name","value":"createUserInput"}},"type":{"kind":"NonNullType","type":{"kind":"NamedType","name":{"kind":"Name","value":"CreateUserInput"}}}}],"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"createUser"},"arguments":[{"kind":"Argument","name":{"kind":"Name","value":"createUserInput"},"value":{"kind":"Variable","name":{"kind":"Name","value":"createUserInput"}}}],"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"id"}},{"kind":"Field","name":{"kind":"Name","value":"email"}},{"kind":"Field","name":{"kind":"Name","value":"firstName"}},{"kind":"Field","name":{"kind":"Name","value":"lastName"}}]}}]}}]} as unknown as DocumentNode<CreateUserMutation, CreateUserMutationVariables>;
---
+++ frontend/src/order/OrderCreateInput.tsx
@@ -0,0 +1,175 @@
+import { z } from "zod";
+import { graphql } from "../ggl";
+import { FC } from "react";
+import { useNavigate } from "react-router";
+import { useMutation, useQuery } from "@apollo/client";
+import { SubmitHandler, useFieldArray, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { FormInput } from "../ui/form/FormInput";
+import { FormSelect } from "../ui/form/FormSelect";
+
+const createOrderMutationDocument = graphql(`
+  mutation CreateOrder($createOrderInput: CreateOrderInput!) {
+    createOrder(createOrderInput: $createOrderInput) {
+      id
+      orderedAt
+      status
+      buyer {
+        id
+        firstName
+        lastName
+        email
+      }
+      farm {
+        id
+        name
+      }
+      orderDetails {
+        id
+        amount
+        produceItem {
+          id
+          name
+        }
+      }
+    }
+  }
+`)
+
+const getFarmRelatedItemQueryDocument = graphql(`
+  query GetFarmRelatedItem($farmId: Float!) {
+    farm(id: $farmId) {
+      id
+      name
+      produceItems {
+        id
+        name
+        produceStock {
+          id
+          amount
+        }
+      }
+    }
+  }
+`)
+
+const createOrderSchema = z.object({
+  produceItems: z.array(
+    z.object({
+      amount: z.coerce.number()
+        .refine((val) => !isNaN(val), {
+          message: '注文数は数値で入力してください',
+        })
+        .refine((val) => val >= 1, {
+          message: '注文数は1以上で入力してください',
+        }),
+      produceItemId: z.coerce.number().min(1, { message: '生産物IDは1以上で入力してください' }),
+    })
+  ),
+})
+
+type CreateOrderInputType = z.output<typeof createOrderSchema>
+
+const OrderCreateInput: FC = () => {
+  const farmId = 1; // TODO: farmIdを動的に取得するように変更する
+  const navigate = useNavigate();
+
+  const { data, loading, error } = useQuery(getFarmRelatedItemQueryDocument, {
+    variables: { farmId: farmId }, // TODO: farmIdを動的に取得するように変更する
+    onError: (error) => {
+      console.error("Error fetching farm data:", error)
+    }
+  })
+
+  // TODO: 自分のfarmIDを決めうちでリクエストしないと今はいけないかも
+  const [createOrder] = useMutation(createOrderMutationDocument, {
+    onCompleted: (data) => {
+      console.log("Order created successfully:", data)
+      navigate("/orders")
+    },
+    onError: (error) => {
+      console.error("Error creating order:", error)
+    }
+  })
+
+
+  const { register, control, handleSubmit, formState: { errors } } = useForm<CreateOrderInputType>({
+    resolver: zodResolver(createOrderSchema),
+    mode: "onBlur",
+    defaultValues: {
+      produceItems: [
+        { amount: 1 },
+      ]
+    },
+  })
+
+  console.log("Form errors:", errors)
+
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: "produceItems",
+  })
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
+  const onSubmit: SubmitHandler<CreateOrderInputType> = (data) => {
+    console.log("Form data:", data)
+    createOrder({
+      variables: {
+        createOrderInput: {
+          farmId: farmId,
+          produceItems: data.produceItems.map(item => ({
+            amount: item.amount,
+            produceItemId: item.produceItemId,
+          })),
+        },
+      },
+    })
+  }
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      {fields.map((item, index) => (
+        <div key={item.id}>
+          <FormSelect<CreateOrderInputType>
+            label="生産物"
+            name={`produceItems.${index}.produceItemId`}
+            register={register}
+            options={(data?.farm?.produceItems ?? []).map((produceItem) => ({
+              value: produceItem.id.toString(),
+              label: produceItem.name,
+            }))}
+            error={errors.produceItems?.[index]?.produceItemId}
+            placeholder="生産物を選択してください"
+          />
+          <FormInput<CreateOrderInputType>
+            label="注文数"
+            name={`produceItems.${index}.amount`}
+            register={register}
+            type="number"
+            placeholder="1"
+            min={0}
+            max={data?.farm?.produceItems?.[index]?.produceStock?.amount || 0}
+            error={errors.produceItems?.[index]?.amount}
+          />
+          <button type="button" onClick={() => remove(index)}>
+            削除
+          </button>
+
+        </div>
+      ))}
+      {(data?.farm?.produceItems && data?.farm?.produceItems.length > 0) && (
+        <button type="button" onClick={() => append({ amount: 1, produceItemId: data?.farm?.produceItems[0]?.id })}>
+          追加
+        </button>
+      )}
+      <button type="submit">
+        注文を作成
+      </button>
+    </form>
+  )
+
+}
+
+export default OrderCreateInput
